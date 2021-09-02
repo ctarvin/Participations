3efb705a8ba5f54e57ef184c3fed81ea3480a893
Cole Tarvin <Cole.Tarvin@ou.edu>
2021-09-02 11:23:22 -0500

    Finished participation

---
+++ Sumof3numbers/Participations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Participations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number. >>");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number. >>");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter your third number. >>");
+            double thirdNumber = Convert.ToDouble(Console.ReadLine());
+         
+            double sum = firstNumber + secondNumber + thirdNumber;
+            const double seven = 7.777;    
+           
+            double finalanswer = sum * seven;
+            Console.WriteLine($"The sum of these three numbers is {sum.ToString("N3")}");
+            Console.WriteLine($"The sum multiplied by 7.777 is {finalanswer.ToString("N3")}");
+        }
+    }
+}
