46b284252b394b77996c3ff143e335156933d474
Cole Tarvin <Cole.Tarvin@ou.edu>
2021-09-09 11:22:22 -0500

    Finished participation

---
+++ Conditional_coinToss/Conditional_coinToss/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Conditional_coinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess heads or tails");
+            string guess = Console.ReadLine();
+            guess = guess.ToLower();
+
+            Random rand = new Random();
+            int randValue = rand.Next(0, 2);
+
+            if (randValue == 0)
+            {
+                if (guess == "heads")
+                {
+                    Console.WriteLine("Heads, you were right!");
+                }
+
+                else if (guess == "tails")
+                {
+                    Console.WriteLine("Heads, better luck next time!");
+                }
+            }
+
+            else if (randValue == 1)
+            {
+                if (guess == "heads")
+                {
+                    Console.WriteLine("Tails, better luck next time!");
+                }
+
+                else if (guess == "tails")
+                    Console.WriteLine("Tails, you were right!");
+                {
+
+                }
+            }
+
+            Console.WriteLine("<<Created by Cole Tarvin>>");
+
+
+        }
+    }
+}
