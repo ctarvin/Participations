7c4c33ec67bb818b53d45a785575392733cb9a76
Cole Tarvin <Cole.Tarvin@ou.edu>
2021-11-30 11:40:46 -0600

    Finished participation

---
+++ Classes-TextBox/Classes-TextBox/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_TextBox
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public string Text { get; set; }
+
+        public TextBox()
+        {
+            Padding = 0;
+            BackColor = Console.BackgroundColor = ConsoleColor.Yellow;
+            ForeColor = Console.ForegroundColor = ConsoleColor.Black;
+            Text = string.Empty;
+
+        }
+        public void DisplayText()
+        {
+            string padding = " ";
+            string output = " ";
+
+            for (int i = 0; i < Padding; i++)
+            {
+                padding += " ";
+            }
+
+            output = padding + Text + padding;
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Classes-TextBox/Classes-TextBox/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classes_TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox text1 = new TextBox();
+
+            text1.Padding = 3;
+            text1.BackColor = Console.BackgroundColor = ConsoleColor.Yellow;
+            text1.ForeColor = Console.ForegroundColor = ConsoleColor.Black;
+            text1.Text = "I completely forgot how to code over the break. I hope this works!";
+            text1.DisplayText();
+
+            TextBox text2 = new TextBox();
+
+            text2.Padding = 5;
+            text2.BackColor = Console.BackgroundColor = ConsoleColor.White;
+            text2.ForeColor = Console.ForegroundColor = ConsoleColor.Blue;
+            text2.Text = "I think I about got it, other than padding.";
+            text2.DisplayText();
+        }
+    }
+}
