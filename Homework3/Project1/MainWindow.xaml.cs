4258c503f381513ede563f245bae525919500435
Cole Tarvin <Cole.Tarvin@ou.edu>
2022-02-15 14:01:24 -0600

    Continued

--- Homework3/Project1/MainWindow.xaml.cs
+++ Homework3/Project1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,13 @@ namespace Project1
         public MainWindow()
         {
             InitializeComponent();
+
+            var lines = File.ReadAllLines("TV Show Data.txt").Skip(1);
+
+            foreach (var line in lines)
+            {
+
+            }
         }
     }
 }
