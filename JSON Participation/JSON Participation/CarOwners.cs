302fe889d8d8b84bef3c26e12c6a2a266797a201
Cole Tarvin <Cole.Tarvin@ou.edu>
2022-02-17 13:06:50 -0600

    bruh

---
+++ JSON Participation/JSON Participation/CarOwners.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Participation
+{
+    internal class CarOwners
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public string Make {get;set;}
+        public string Model {get;set;}
+        public string Year {get;set;}
+        public string Color { get; set; }
+
+
+        public CarOwners()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Make     = string.Empty;
+            Model    = string.Empty;
+            Year     = string.Empty;
+            Color = string.Empty;
+        }
+
+        public CarOwners(string owner)
+        {
+            var info = owner.Split();
+
+            FirstName = info[1];
+            LastName = info[2];
+            Make = info[3];
+            Model = info[4];
+            Year = info[5];
+            Color = info[6];
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} owns a {Color} {Year} {Make} {Model}";
+        }
+
+    }
+}
---
+++ JSON Participation/JSON Participation/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Participation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<CarOwners> car = new List<CarOwners>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string json = File.ReadAllText("Mock_Data_Car_Owners.json");
+
+            var results = JsonConvert.DeserializeObject<List<CarOwners>>(json);
+
+            foreach (CarOwners owner in results)
+            {
+                car.Add(new CarOwners(owner.ToString()));
+                if (cboColors.Items.Contains(owner.Color) == false)
+                {
+                    cboColors.Items.Add(owner.Color);
+                }
+            }
+
+            PopulateListBox(car);
+            
+        }
+        private void PopulateColorBox()
+        {
+            cboColors.SelectedIndex = 12;
+        }
+
+        private void PopulateListBox(List<CarOwners> carOwners)
+        {
+            lstData.Items.Clear();
+            foreach (var car in carOwners)
+            {
+                lstData.Items.Add(car);
+            }
+        }
+
+        private void cboColors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lstData.Items.Clear();
+            foreach (CarOwners owners in car)
+            {
+                if (owners.Color = cboColors.SelectedItem)
+                {
+                    lstData.Items.Add(owners.Color);
+                }
+            }
+        }
+    }
+}
