086fa2774af14d6c77e94e585734cfa8487bc014
Cole Tarvin <Cole.Tarvin@ou.edu>
2022-04-14 12:59:14 -0500

    started participation

---
+++ MVC_Participations/MVC Beginner/Controllers/PokemonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Beginner.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+
+            using (var client = new HttpClient())
+            {
+                var json = client.GetStringAsync("https://api.chucknorris.io/jokes/random").Result;
+
+                joke = JsonConvert.DeserializeObject<RandomChuckJoke>(json);
+            }
+            return View();
+        }
+    }
+}
